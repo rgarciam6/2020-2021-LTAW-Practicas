1ed3e179bc8d8599663e9f8035b93db5bc18e9a6
rgarciam6 <rebeca.gm1996@gmail.com>
2021-05-16 20:53:59 +0200

    Añadiendo mejora sonido

--- P3/public/chat-client.js
+++ P3/public/chat-client.js
@@ -1,13 +1,14 @@
 //-- Elementos del interfaz
 const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
-
+let sonido = new Audio('sound.mp3');
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
 
 
 socket.on("message", (msg)=>{
   display.innerHTML += '<p>' + msg + '</p>';
+  sonido.play();
 });
 
 //-- Al apretar el botón se envía un mensaje al servidor
