b8cd89389bb85c2db993811c00ea79164a24ae4a
rgarciam6 <rebeca.gm1996@gmail.com>
2021-05-17 19:06:45 +0200

    Cambiando archivos para empezar P4

---
+++ P4/public/chat-client.js
@@ -0,0 +1,21 @@
+//-- Elementos del interfaz
+const display = document.getElementById("display");
+const msg_entry = document.getElementById("msg_entry");
+let sonido = new Audio('sound.mp3');
+//-- Crear un websocket. Se establece la conexión con el servidor
+const socket = io();
+
+
+socket.on("message", (msg)=>{
+  display.innerHTML += '<p>' + msg + '</p>';
+  sonido.play();
+});
+
+//-- Al apretar el botón se envía un mensaje al servidor
+msg_entry.onchange = () => {
+  if (msg_entry.value)
+    socket.send(msg_entry.value);
+  
+  //-- Borrar el mensaje actual
+  msg_entry.value = "";
+}
